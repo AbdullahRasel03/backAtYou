fabb789d56f6efab3cdb35f2b438ad66a0cfbd19
Pixel-Man360 <muntasir.chowdury360@gmail.com>
2022-07-20 23:35:01 +0600

    Player Hit Effect Added

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,8 +9,9 @@ public class Player : MonoBehaviour, IDamageable
     public static event Action<int> OnHealthChanged;
     [SerializeField] private PlayerData data;
     [SerializeField] private Camera cam;
+    [SerializeField] private Material playerHitMat;
 
-
+    private IEnumerator hitEffectCoroutine;
     private int currentHealth;
 
     void Start()
@@ -23,10 +24,35 @@ public class Player : MonoBehaviour, IDamageable
         currentHealth -= amount;
         OnHealthChanged?.Invoke(currentHealth);
 
+        playerHitMat.SetFloat("_FullScreenIntensity", 0.4f);
+
+        if (hitEffectCoroutine != null)
+        {
+            StopCoroutine(hitEffectCoroutine);
+        }
+
+        hitEffectCoroutine = RemoveHitEffect();
+
+        StartCoroutine(hitEffectCoroutine);
+
         if (currentHealth <= 0)
         {
             PopupController.GetInstance().popupLevelLost.ShowView();
             OnPlayerDead?.Invoke();
+        }
+    }
+
+    private IEnumerator RemoveHitEffect()
+    {
+        float val = 0.4f;
+
+        while (playerHitMat.GetFloat("_FullScreenIntensity") != 0f)
+        {
+            val -= Time.deltaTime * 0.6f;
+            val = Mathf.Clamp(val, 0f, 0.4f);
+            playerHitMat.SetFloat("_FullScreenIntensity", val);
+
+            yield return null;
         }
     }
 
@@ -39,4 +65,11 @@ public class Player : MonoBehaviour, IDamageable
     {
         return this.cam;
     }
+
+    void OnDestroy()
+    {
+        playerHitMat.SetFloat("_FullScreenIntensity", 0f);
+    }
+
+
 }
